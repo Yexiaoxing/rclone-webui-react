8b40662c9dcc313c60757a78f5cf4cdcff423e44
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-30 02:28:58 +0000

    Deploy rclone/rclone-webui-react to github.com/rclone/rclone-webui-react.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.489a2fc08c90106e3bd23cb5b40969d6.js"
+  "./precache-manifest.074349055aacb78bbaa457bddf194e11.js"
 );
 
 self.addEventListener('message', (event) => {
